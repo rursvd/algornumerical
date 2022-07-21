3a4a2e94d6be3f3e7c1955c1e44716b0642f5fef
rursvd <31153122+rursvd@users.noreply.github.com>
2022-07-21 16:53:33 +0900

    Create ch7-2.py

---
+++ ch7-2.py
@@ -0,0 +1,11 @@
+# Example 	
+import numpy as np
+
+A = np.array([[5,6],[9,7],[3,5],[1,2]])
+
+a13 = np.sum(A[0])
+a23 = np.sum(A[1])
+a33 = np.sum(A[2])
+a43 = np.sum(A[3])
+
+print(a13,a23,a33,a43)
