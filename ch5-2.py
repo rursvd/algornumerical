51e81a6a2dc32031f5bb3d3001b903aee2f57cea
rursvd <31153122+rursvd@users.noreply.github.com>
2023-09-29 10:37:12 +0900

    Update ch5-2.py

--- ch5-2.py
+++ ch5-2.py
@@ -3,5 +3,6 @@ xold = 1
 
 for k in range(N):
     xnew = 0.4*xold + 0.1
-    print(f'{k+1},x={xnew:0.4f}')
+    err = abs((xnew - xold)/xnew) * 100
+    print(f'{k+1},x={err:0.4f} %')
     xold = xnew  
